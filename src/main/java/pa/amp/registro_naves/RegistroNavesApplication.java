fcf9b2b509dc2aba9bb8b684d2aa1e680839fff7
lowilt24 <84353110+lowilt24@users.noreply.github.com.>
2026-09-04 15:48:09 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/pa/amp/registro_naves/RegistroNavesApplication.java
@@ -0,0 +1,13 @@
+package pa.amp.registro_naves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroNavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroNavesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pa/amp/registro_naves/RegistroNavesApplicationTests.java
@@ -0,0 +1,13 @@
+package pa.amp.registro_naves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroNavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
